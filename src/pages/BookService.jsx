1d26bfa123974521ed93fc3dcb158ba6f4f4d46c
Barun Nafak <bnafak@gmail.com>
2023-12-21 19:08:15 +0600

    booking added

--- src/pages/BookService.jsx
+++ src/pages/BookService.jsx
@@ -5,13 +5,12 @@ import { useContext } from "react";
 
 const BookService = () => {
   const service = useLoaderData();
-  const { title, _id, price } = service;
+  const { title, _id, price, img } = service;
   const { user } = useContext(AuthContext);
 
   const handleBookService = (event) => {
-    
     event.preventDefault();
-    
+
     const form = event.target;
     const name = form.name.value;
     // const email = user?.email;
@@ -19,19 +18,32 @@ const BookService = () => {
     const date = form.date.value;
     const price = form.price.value;
 
-    const order = {
+    const booking = {
       customerName: name,
       email,
+      //img,
       date,
-      //service: _id,
-      price
+      service: title,
+      service_id: _id,
+      price,
     };
-    console.log(order);
+    console.log(booking);
+    fetch("http://localhost:5000/bookings", {
+      method : "POST",
+      headers : {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(booking)
+    })
+      .then(res => res.json())
+      .then(data => {
+        console.log(data);
+      });
   };
 
   return (
     <div>
-      <h2 className="text-center text-3xl">Book serviece :{title} </h2>
+      <h2 className="text-center text-3xl">Book services :{title} </h2>
       <form onSubmit={handleBookService}>
         <div className="grid grid-cols-11 md:grid-cols-2 gap-6">
           <div className="form-control">
@@ -40,22 +52,16 @@ const BookService = () => {
             </label>
             <input
               type="text"
-              name="name"
               defaultValue={user?.displayName}
-              placeholder="Name"
+              name="name"
               className="input input-bordered"
-              required
             />
           </div>
           <div className="form-control">
             <label className="label">
               <span className="label-text">Date</span>
             </label>
-            <input
-              type="date" name="date"
-              className="input input-bordered"
-              required
-            />
+            <input type="date" name="date" className="input input-bordered" />
           </div>
 
           <div className="form-control">
@@ -77,8 +83,8 @@ const BookService = () => {
             </label>
             <input
               type="type"
-              defaultValue= {price}
               name="price"
+              defaultValue={"$" + price}
               className="input input-bordered"
               required
             />
